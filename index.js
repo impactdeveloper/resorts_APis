8270110d89301ac9511b834c6d136016206ca8bd
adnan bashir <adnanbashir3358@gmail.com>
2022-09-26 13:47:59 +0400

    minor

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors());
 app.get('/', function (req, res) {
-    res.send("hello world");
+    res.send("Azure App successfully working");
 });
 
 app.use('/uploads',express.static('uploads'))
